665db3f1dc6a5f6d0c57ba00d2174086f895d974
bushan nagendra <bushan1891@gmail.com>
2016-10-10 15:17:20 -0700

    cart completed

--- ReactAuthApp/src/components/Cart/cart_index.js
+++ ReactAuthApp/src/components/Cart/cart_index.js
@@ -2,18 +2,38 @@ import { CART_ITEM , NEW_CART_ITEM , NEW_CART} from '../Table/viewtable/edittabl
 import React , { Component} from 'react';
 import {connect} from 'react-redux';
 import styles from './styles.css';
+import Cart_Edit from './cart-edit';
+
 
 class Cart_Index extends Component{
-	
+
+renderTable(table){
+			if(table){
+				return(
+					<Cart_Edit data={table} key={table._id}/>
+					)
+			}
+}	
+
+renderTest(obj){
+console.log('test',obj);
+}
+
 render(){
+	const cart = this.props.cart;
+	if(cart.table.length==0){
+		swal('Cart Is empty');
+	}
+
 	return(
 		<div className="">
 		<div className={styles.title}>Current Cart Elements</div>
-			<div className={styles.flex_container}>
-			</div>	
-	
+			<div className={styles.cart_table_container}>
+			{cart.table.map((obj)=>this.renderTable(obj))}
+			</div>		
 		</div>
 		)
+
 	}
 }
 
